e8bb0c41f5c60a7376b914e5d2cd31edef64ef68
Abhinav Nambi <abhinavnambi09@Gmail.com>
2026-08-15 17:33:20 +0530

    debugging

--- excelreader.m
+++ excelreader.m
@@ -4,8 +4,8 @@ function [hp_front, hp_rear] = excelreader(input,loaded_radius)
     hp_front=struct('LF',zeros(1,3),'LA',zeros(1,3),'LBJ',zeros(1,3),'UF',zeros(1,3),'UA',zeros(1,3),'UBJ',zeros(1,3),'PRO',zeros(1,3),'PRI',zeros(1,3),'TRO',zeros(1,3),'TRI',zeros(1,3),'DC',zeros(1,3),'RD',zeros(1,3),'WSP',zeros(1,3),'WC',zeros(1,3),'RPA1',zeros(1,3),'RPA2',zeros(1,3));
     keys=fieldnames(hp_front);
     for i = 1:numel(keys)
-        hp_front.(keys{i})=(data_front(i,:) .* 0.001);
-        hp_front.(keys{i})=(data_front(i,:) * [1;-1;1]);
+        hp_front.(keys{i})=(data_front(i,:));
+        hp_front.(keys{i})=(data_front(i,:) * [[1,0,0];[0,-1,0];[0,0,1]]);
     end
     
     z_ground=(hp_front.WC(3)-loaded_radius);
@@ -18,8 +18,8 @@ function [hp_front, hp_rear] = excelreader(input,loaded_radius)
     hp_rear=struct('LF',zeros(1,3),'LA',zeros(1,3),'LBJ',zeros(1,3),'UF',zeros(1,3),'UA',zeros(1,3),'UBJ',zeros(1,3),'PRO',zeros(1,3),'PRI',zeros(1,3),'TRO',zeros(1,3),'TRI',zeros(1,3),'DC',zeros(1,3),'RD',zeros(1,3),'WSP',zeros(1,3),'WC',zeros(1,3),'RPA1',zeros(1,3),'RPA2',zeros(1,3));
     keys=fieldnames(hp_rear);
     for i = 1:numel(keys)
-        hp_rear.(keys{i})=(data_rear(i,:) .* 0.001);
-        hp_rear.(keys{i})=(data_rear(i,:) * [1;-1;1]);
+        hp_rear.(keys{i})=(data_rear(i,:));
+        hp_rear.(keys{i})=(data_rear(i,:) * [[1,0,0];[0,-1,0];[0,0,1]]);
     end
 
     z_ground=(hp_rear.WC(3)-loaded_radius);
