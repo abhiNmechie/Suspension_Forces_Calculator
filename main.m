d78fcf2890cc61afb0b30eaa3f3c4bf0264a7335
Abhinav Nambi <abhinavnambi09@Gmail.com>
2026-08-18 17:17:42 +0530

    fruntknuckle

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 clear; clc; close all;
 format long g;
 [gen] = generalinputs();
-[hp_front,hp_rear]=excelreader("C:\MechX VD\force calculator\hardpoints.csv",234.80);
+[hp_front,hp_rear]=excelreader("C:\MechX VD\force calculator\Suspension forces calculator\hardpoints.csv",234.80);
 [geo]=geometry(hp_front,hp_rear);
 [result]=frontknuckle(gen,geo);
 disp(result.F_UWF_brake);
