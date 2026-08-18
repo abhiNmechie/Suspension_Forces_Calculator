a236a930d2ee61e109b2217c85676c8fc5e19d50
Abhinav Nambi <abhinavnambi09@Gmail.com>
2026-08-18 17:17:56 +0530

    wrote force balance

--- main.m
+++ main.m
@@ -4,5 +4,5 @@ format long g;
 [hp_front,hp_rear]=excelreader("C:\MechX VD\force calculator\Suspension forces calculator\hardpoints.csv",234.80);
 [geo]=geometry(hp_front,hp_rear);
 [result]=frontknuckle(gen,geo);
-disp(result.F_UWF_brake);
-disp(result.F_LWF_brake);
+disp(result.Front_UWF_brake);
+disp(result.Front_LWF_brake);
