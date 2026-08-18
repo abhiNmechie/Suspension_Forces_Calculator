29a75fb21419d5b7b66a10985f506b999912d1ad
Abhinav Nambi <abhinavnambi09@Gmail.com>
2026-08-18 17:59:55 +0530

    debugged brake case frontknuckle solver

--- frontknuckle.m
+++ frontknuckle.m
@@ -1,11 +1,17 @@
-function [result] = frontknuckle(gen, geo)
+function [result] = frontknuckle (gen, geo)
     
-    %braking forces:
-    geo.wheelbase=((geo.wheelbase)/1000);
-    gen.h=((gen.h)/1000);
-    result.Fw=(gen.Mf)*((gen.g)/2);
-    result.delta_F=(gen.M)*(gen.deacc)*((gen.h)/(2*(geo.wheelbase)));
-    result.total_front_load=((result.Fw)+(result.delta_F));
-    result.friction=(gen.mu)*(result.total_front_load);
-    result.Front_LWF_brake=(((result.friction)*(gen.loaded_radius-geo.upphubsep_front))/(geo.upphubsep_front+geo.lowhubsep_front));
-    result.Front_UWF_brake=(result.friction+result.Front_LWF_brake);
+    L=(geo.wheelbase/1000);
+    h=(gen.h/1000);
+    a=(geo.upphubsep_front/1000);
+    b=(geo.lowhubsep_front/1000);
+    r=(gen.loaded_radius/1000);
+
+    %braking
+    result.static_frontload=(gen.Mf)*((gen.g)/2);
+    result.front_WT_brake=((gen.M)*(gen.deacc)*(h))/(2*L);
+    result.front_total_brake=(result.static_frontload+result.front_WT_brake);
+    result.friction_front_brake=((gen.mu)*(result.front_total_brake));
+
+    result.UWF_brake=((result.friction_front_brake*b-result.friction_front_brake*r)/(a+b));
+    result.LWF_brake=(result.friction_front_brake-result.UWF_brake);
+end
--- main.m
+++ main.m
@@ -4,5 +4,3 @@ format long g;
 [hp_front,hp_rear]=excelreader("C:\MechX VD\force calculator\Suspension forces calculator\hardpoints.csv",234.80);
 [geo]=geometry(hp_front,hp_rear);
 [result]=frontknuckle(gen,geo);
-disp(result.Front_UWF_brake);
-disp(result.Front_LWF_brake);
